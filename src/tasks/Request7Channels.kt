250962b30da24b2ad768ca20710f11494d924ca9
Alimjan Qadir <38877166+alimjanqadir@users.noreply.github.com>
2023-03-07 21:07:52 +0800

    Task 7 is completed.

--- src/tasks/Request7Channels.kt
+++ src/tasks/Request7Channels.kt
@@ -1,6 +1,8 @@
 package tasks
 
 import contributors.*
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.async
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
@@ -11,6 +13,27 @@ suspend fun loadContributorsChannels(
     updateResults: suspend (List<User>, completed: Boolean) -> Unit
 ) {
     coroutineScope {
-        TODO()
+        val repos = service
+            .getOrgRepos(req.org)
+            .also { logRepos(req, it) }
+            .body() ?: emptyList()
+
+        val channel = Channel<List<User>>()
+        var allUsers = emptyList<User>()
+        for (repo in repos) {
+            launch {
+                val users = service
+                    .getRepoContributors(req.org, repo.name)
+                    .also { logUsers(repo, it) }
+                    .bodyList()
+                channel.send(users)
+            }
+        }
+
+        repeat(repos.size) {
+            val users = channel.receive()
+            allUsers = (allUsers + users).aggregate()
+            updateResults(allUsers, it == repos.lastIndex)
+        }
     }
 }
